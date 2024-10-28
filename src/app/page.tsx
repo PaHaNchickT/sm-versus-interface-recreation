3b0638c6ce7db85c22b023972d1ca588cb1bb8d9
Pavel <you@example.com>
2024-10-28 13:54:04 +0300

    feat: implement start screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 'use client';
 
-import { type ReactElement } from 'react';
+import { Button } from '@nextui-org/react';
+import { useState, type ReactElement } from 'react';
+
+import Villains from '@/components/Villains/Villains';
 
 const App = (): ReactElement => {
-  return <p>Page</p>;
+  const [clicked, setClicked] = useState(false);
+  return (
+    <>
+      <main className="self-center">
+        {clicked ? (
+          <Villains />
+        ) : (
+          <Button size="lg" color="danger" onPress={() => setClicked(true)}>
+            Start
+          </Button>
+        )}
+      </main>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Villains/Villains.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { type ReactElement } from 'react';
+
+const Villains = (): ReactElement => {
+  return <p>Villains</p>;
+};
+
+export default Villains;
