1715bad4a3c60f54ee8164c06e9dbf51c6690609
Pavel <you@example.com>
2024-10-28 16:11:07 +0300

    fix: remove old text content module from general files

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,18 +3,16 @@
 import { Button } from '@nextui-org/react';
 import type { ReactElement } from 'react';
 
-import { TEXT_CONTENT } from '@/constants/const-text-content';
-
 const Error = (): ReactElement => {
   return (
     <div className="flex justify-center items-center text-center">
       <div className="flex flex-col z-[2] gap-5 items-center">
         <div>
-          <p className="text-3xl text-center sm:text-4xl">{TEXT_CONTENT.error.errorTitle}</p>
-          <p className="text-center text-xl sm:text-2xl">{TEXT_CONTENT.error.errorMsg}</p>
+          <p className="text-3xl text-center sm:text-4xl">Oops!</p>
+          <p className="text-center text-xl sm:text-2xl">Something went wrong...</p>
         </div>
         <Button onClick={() => location.reload()} color="danger">
-          {TEXT_CONTENT.error.reloadBtn}
+          Reload
         </Button>
       </div>
     </div>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,8 +4,6 @@ import { Button } from '@nextui-org/react';
 import { useRouter } from 'next/navigation';
 import type { ReactElement } from 'react';
 
-import { TEXT_CONTENT } from '@/constants/const-text-content';
-
 export default function NotFound(): ReactElement {
   const router = useRouter();
 
@@ -13,11 +11,11 @@ export default function NotFound(): ReactElement {
     <div className="flex justify-center items-center text-center">
       <div className="flex flex-col z-[2] gap-5 items-center">
         <div>
-          <p className="text-3xl text-center sm:text-4xl">{TEXT_CONTENT.error.notFoundTitle}</p>
-          <p className="text-sm sm:text-xl">{TEXT_CONTENT.error.notFoundMsg}</p>
+          <p className="text-3xl text-center sm:text-4xl">404</p>
+          <p className="text-sm sm:text-xl">Page not found...</p>
         </div>
         <Button onClick={() => router.push('/')} color="danger">
-          {TEXT_CONTENT.error.homeBtn}
+          Home
         </Button>
       </div>
     </div>
