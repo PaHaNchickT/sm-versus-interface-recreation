42b760bc598c15fc639ad9924e8df51ce661491b
Pavel <you@example.com>
2024-10-28 16:37:37 +0300

    feat: implement styles for control panel

---
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import Image from 'next/image';
+import { type ReactElement } from 'react';
+
+import { CONTROL_PANEL } from '@/constants/constants';
+
+const ControlPanel = (): ReactElement => {
+  return (
+    <div className="flex gap-10 relative">
+      <div className="absolute w-full h-full bg-gradient-to-r from-[#272a29] via-[#7c7c7c] to-[#272a29] [mask-image:url('/images/mask.png')] [mask-repeat:no-repeat] [mask-size:cover]"></div>
+      {CONTROL_PANEL.map((item, index) => (
+        <div key={index} className="flex gap-3 items-center relative z-[1]">
+          <Image
+            src={item.img}
+            width={24}
+            height={24}
+            alt={`button-${index}`}
+            className="shadow-[0_0_2px_1px_white] rounded-full"
+          />
+          <p>{item.text}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ControlPanel;
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -11,13 +11,28 @@ export const VILLAINS_PATH = [
 ];
 
 export const VILLAINS_STYLES = [
-  '/images/versus-art-scorpion.png',
-  '/images/versus-art-hulk.png',
-  '/images/versus-art-goblin.png',
-  '/images/versus-art-unknown.png',
-  '/images/versus-art-jug.png',
-  '/images/versus-art-kraven.png',
-  '/images/versus-art-cat.png',
-  '/images/versus-art-carnage.png',
-  '/images/versus-art-robot.png',
+  'left-[calc((100%-840px)/2+360px)]',
+  'left-[calc((100%-840px)/2+270px)]',
+  'left-[calc((100%-840px)/2+180px)]',
+  'left-[calc((100%-840px)/2+90px)]',
+  'left-[calc((100%-840px)/2)]',
+  'left-[calc((100%-840px)/2-90px)]',
+  'left-[calc((100%-840px)/2-180px)]',
+  'left-[calc((100%-840px)/2-270px)]',
+  'left-[calc((100%-840px)/2-360px)]',
+];
+
+export const CONTROL_PANEL = [
+  {
+    text: 'continue',
+    img: '/images/btn-A.png',
+  },
+  {
+    text: 'boss info',
+    img: '/images/btn-Y.png',
+  },
+  {
+    text: 'back',
+    img: '/images/btn-B.png',
+  },
 ];
